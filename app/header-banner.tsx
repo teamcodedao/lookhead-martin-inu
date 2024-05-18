a28254312bf98effedf5753861f6f8e157fbf54c
Nghiep <me@nghiep.dev>
2024-05-18 12:40:15 +0700

    Add banner intro

---
+++ app/header-banner.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import Image from 'next/image';
+
+import logo from './logo.png';
+
+export default function HeaderBanner() {
+  return (
+    <>
+      <header className='relative'>
+        <video
+          autoPlay
+          muted
+          loop
+          playsInline
+          preload='auto'
+          className='w-full object-cover'
+        >
+          <source src='/intro-1.mp4' type='video/mp4' />
+        </video>
+        <div className='absolute inset-0 px-5 pt-10 sm:px-10 lg:pt-20'>
+          <Image
+            src={logo}
+            priority
+            quality={100}
+            alt=''
+            className='mx-auto block max-w-[min(1160px,100%)]'
+          />
+        </div>
+      </header>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,76 +1,11 @@
 import Image from 'next/image';
 
-import image1 from './image-1.png';
-import image2 from './image-2.png';
-import image3 from './image-3.png';
-import image4 from './image-4.png';
-import image5 from './image-5.png';
-import image6 from './image-6.png';
+import HeaderBanner from './header-banner';
 
 export default function Home() {
   return (
-    <main className='min-h-screen text-white'>
-      <div className='relative'>
-        <Image
-          src={image1}
-          quality={100}
-          priority
-          alt=''
-          className='max-h-screen w-full object-cover object-top'
-        />
-        <div className='absolute inset-0'>
-          <h1 className='mt-10 px-5 text-center text-5xl font-bold'>
-            LockheedMartinInu
-          </h1>
-          <div className='absolute bottom-[-370px] flex w-full justify-center'>
-            <Image
-              src={image2}
-              quality={100}
-              width={1105}
-              priority
-              alt=''
-              className='max-w-[90%]'
-            />
-          </div>
-          <div className='absolute bottom-[-420px] left-1/2 flex w-full max-w-screen-desktop -translate-x-1/2 justify-between overflow-hidden sm:bottom-[-470px] md:bottom-[-540px]'>
-            <Image
-              src={image3}
-              quality={100}
-              width={509}
-              priority
-              alt=''
-              className='max-w-[35%]'
-            />
-            <Image
-              src={image4}
-              quality={100}
-              width={379}
-              priority
-              alt=''
-              className='max-w-[30%]'
-            />
-          </div>
-        </div>
-      </div>
-
-      <div>
-        <Image
-          src={image5}
-          quality={100}
-          priority
-          alt=''
-          className='max-h-screen w-full object-cover object-top'
-        />
-      </div>
-      <div>
-        <Image
-          src={image6}
-          quality={100}
-          priority
-          alt=''
-          className='max-h-screen w-full object-cover object-top'
-        />
-      </div>
+    <main className='min-h-screen'>
+      <HeaderBanner />
     </main>
   );
 }
